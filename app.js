654f1c6c9e68b3ab83b2f1428586ec4bdaa5a0f0
Luis Felipe <l_felipelizeu@hotmail.com>
2021-10-16 00:19:01 -0300

    Created a post request to /games. The body request name can't be blank or repeated, the stockTotal, categoryId and pricePerDay must be and integer positive number. The categoryId also must exist on the categories table.

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('/categories', async (req, res) => {
         const categories = await connection.query('SELECT * FROM categories;');
 
         res.status(200).send(categories.rows);
-    } catch (error) {
+    } catch {
         res.sendStatus(500);
     }
 });
@@ -43,7 +43,7 @@ app.post('/categories', async (req, res) => {
         await connection.query('INSERT INTO categories (name) VALUES ($1)', [newCategoryName]);
 
         res.sendStatus(201);
-    } catch (error) {
+    } catch {
         res.sendStatus(500);
     }
 });
@@ -75,8 +75,45 @@ app.get('/games', async (req, res) => {
                     ON games."categoryId" = categories.id;`);
 
         res.status(200).send(games.rows);
-    } catch (error) {
-        console.log(error);
+    } catch {
+        res.sendStatus(500);
+    }
+});
+
+app.post('/games', async (req, res) => {
+    const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
+
+    const userSchema = joi.object({
+        name: joi.string().empty().trim().required(),
+        stockTotal: joi.number().integer().positive().required(),
+        pricePerDay: joi.number().integer().positive().required(),
+        categoryId: joi.number().integer().positive().required(),
+    });
+
+    try {
+        const { error } = userSchema.validate({
+            name,
+            stockTotal,
+            categoryId,
+            pricePerDay,
+        });
+
+        const categories = await connection.query('SELECT * FROM categories WHERE id = $1;', [categoryId]);
+        if (joi.isError(error) || categories.rows.length === 0) {
+            res.sendStatus(400);
+            return;
+        }
+
+        const games = await connection.query('SELECT * FROM games WHERE name = $1', [name.trim()]);
+        if (games.rows.length !== 0) {
+            res.sendStatus(409);
+            return;
+        }
+
+        await connection.query(`INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") VALUES ($1,$2,$3,$4,$5);`, [name.trim(), image, stockTotal, categoryId, pricePerDay]);
+
+        res.sendStatus(201);
+    } catch {
         res.sendStatus(500);
     }
 });
