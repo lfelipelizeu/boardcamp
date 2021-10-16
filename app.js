bb674d387b44f391b4afe7aec95c4089e79ee264
Luis Felipe <l_felipelizeu@hotmail.com>
2021-10-15 22:17:58 -0300

    Create a post request to /categories. The body request must have the name of the category, cannot be empty or repeated.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import joi from 'joi';
 import connection from './database.js';
 
 const app = express();
@@ -11,6 +12,37 @@ app.get('/categories', async (req, res) => {
         const categories = await connection.query('SELECT * FROM categories;');
 
         res.status(200).send(categories.rows);
+    } catch (error) {
+        res.sendStatus(500);
+    }
+});
+
+app.post('/categories', async (req, res) => {
+    const newCategoryName = req.body.name.trim();
+
+    const userSchema = joi.object({
+        name: joi.string().empty()
+    });
+
+    try {
+        const { error } = userSchema.validate({
+            name: newCategoryName,
+        });
+
+        if (joi.isError(error)) {
+            res.sendStatus(400);
+            return;
+        }
+        
+        const categories = await connection.query('SELECT * FROM categories;');
+        if (categories.rows.some(category => category.name === newCategoryName)) {
+            res.sendStatus(409);
+            return;
+        }
+
+        await connection.query('INSERT INTO categories (name) VALUES ($1)', [newCategoryName]);
+
+        res.sendStatus(201);
     } catch (error) {
         res.sendStatus(500);
     }
